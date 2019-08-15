746130d87831ce3bc6843f727f967b92018249a0
thebinaryfelix <mateus.cfoliveira@gmail.com>
2019-08-15 11:56:55 -0300

    feat(swagger): add swagger documentation options

---
+++ api/config/swagger.js
@@ -0,0 +1,26 @@
+// Followed configuration from package documentation
+// See more at: https://www.npmjs.com/package/express-swagger-generator#usage
+
+module.exports = {
+  swaggerDefinition: {
+    info: {
+      description: 'This is a sample server',
+      title: 'Swagger',
+      version: '1.0.0',
+    },
+    host: 'localhost:3000',
+    basePath: '/v1',
+    produces: ['application/json', 'application/xml'],
+    schemes: ['http', 'https'],
+    securityDefinitions: {
+      JWT: {
+        type: 'apiKey',
+        in: 'header',
+        name: 'Authorization',
+        description: '',
+      },
+    },
+  },
+  basedir: __dirname, //app absolute path
+  files: ['../routes/**/*.js'], //Path to the API handle folder
+}
