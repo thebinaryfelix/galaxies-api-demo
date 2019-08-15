36d74e15c0ffa7cdd732b012c024366fec508c49
thebinaryfelix <mateus.cfoliveira@gmail.com>
2019-08-15 11:56:34 -0300

    refactor(app): setup swagger generator and add console to mongo connection

--- api/app.js
+++ api/app.js
@@ -1,19 +1,30 @@
 // Import dependencies
 const express = require('express')
 const mongoose = require('mongoose')
-const routes = require('./routes')
+const expressSwaggerGenerator = require('express-swagger-generator')
 
-// Mount server
-const app = express()
+// Import project modules
+const swaggerOptions = require('./config/swagger')
+const routes = require('./routes')
 
 // Configure environment
 const PORT = process.env.PORT
+const DATABASE_URL = process.env.DATABASE_URL
 
-mongoose.connect('mongodb://localhost:27017/galaxiesApi', {
-  useNewUrlParser: true,
-})
+// Setup Express and Swagger
+const app = express()
+const expressSwagger = expressSwaggerGenerator(app)
+expressSwagger(swaggerOptions)
+
+// Establish connection with database
+mongoose
+  .connect(DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log('Connected to mongo!'))
+  .catch(err => console.error('Error connecting to mongo', err))
 
 //  Connect all our routes to the application
 app.use('/', routes)
 
-app.listen(PORT, () => console.log(`Simple API listening on port: ${PORT}!`))
+app.listen(PORT, () => console.log(`Galaxies API listening on port: ${PORT}!`))
