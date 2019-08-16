012dba0389fb329b2564a7b446a51e9427572063
Mateus Felix <mateus.felix@NOT-M000634.future.corp>
2019-08-16 18:03:21 -0300

    docs(galaxy.routes): add docs to route for single galaxy

--- api/routes/galaxy.routes.js
+++ api/routes/galaxy.routes.js
@@ -5,21 +5,28 @@ const router = express.Router()
 
 /**
  * @route GET /galaxies/all
- * @group Galaxies
+ * @group Galaxies - Operations involving all documents
  * @returns {Array.<Galaxy>} 200 - An array of all galaxies
- * @returns {Error.model}  403 - Forbidden
+ * @returns {Error.model}  400 - Bad request
  */
 router.get('/galaxies/all', getAllGalaxies)
 
 /**
  * @route POST /galaxies
- * @group Galaxies
+ * @group Galaxies - Operations involving all documents
  * @param {Galaxy.model} galaxy.body.required - Galaxy object
  * @returns {Galaxy.model} 200 - The galaxy saved
- * @returns {Error.model}  403 - Forbidden
+ * @returns {Error.model}  400 - Bad request
  */
 router.post('/galaxies', addGalaxy)
 
+/**
+ * @route GET /galaxy/{code}
+ * @group Galaxy - Operations involving a single document
+ * @param {string} code.path.required - Unique code (lowercase) that defines that galaxy in a specific catalogue - eg: m31
+ * @returns {Galaxy.model} 200 - A single galaxy
+ * @returns {Error.model}  400 - Bad request
+ */
 router.get('/galaxy/:code', getGalaxyByCode)
 
 module.exports = router
