32d0a2029a1e1c6a5b951c67ee20742d51afd0cc
Mateus Felix <mateus.felix@NOT-M000634.future.corp>
2019-08-16 17:18:42 -0300

    refactor(galaxy.model): define galaxy model for swagger doc

--- api/models/galaxy.model.js
+++ api/models/galaxy.model.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+
+const { Schema } = mongoose
+
+/**
+ * @typedef Galaxy
+ * @property {enum} catalogue.required - specify which catalogue it is - eg: messier, ngc
+ * @property {string} code.required - Galaxy code on the catalogue
+ * @property {string} name.required - Popular name
+ * @property {integer} size.required - Diameter in Light Years - eg: 100000
+ * @property {enum} type.required - Galaxy structure type - eg: elliptical, spiral, irregular
+ */
 
 const galaxySchema = new Schema(
   {
@@ -10,8 +20,12 @@ const galaxySchema = new Schema(
     },
     code: { type: String, required: [true, 'Galaxy code is required'] },
     name: { type: String, required: [true, 'Galaxy name is required'] },
-    size: { type: Number, default: 'Unknown' },
-    type: { type: String, required: [true, 'Galaxy type is required'] },
+    size: { type: Number, required: [true, 'The galaxy must have a size'] },
+    type: {
+      type: String,
+      enum: ['elliptical', 'spiral', 'irregular'],
+      required: [true, 'Galaxy type is required'],
+    },
   },
   {
     timestamps: {
