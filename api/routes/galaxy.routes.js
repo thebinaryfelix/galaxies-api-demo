3ab3c1e93fc67b635fdb4ffafec610bb5ed72310
thebinaryfelix <mateus.cfoliveira@gmail.com>
2019-08-16 11:12:40 -0300

    refactor(galaxy.routes): add documentation

--- api/routes/galaxy.routes.js
+++ api/routes/galaxy.routes.js
@@ -3,7 +3,15 @@ const { getGalaxies, addGalaxy } = require('../controllers/galaxy.controller')
 
 const router = express.Router()
 
+/**
+ * This function comment is parsed by doctrine
+ * @route GET /galaxies
+ * @group CRUD - Galaxy
+ * @returns {object} 200 - An array of all galaxies info
+ * @returns {Error}  403 - Forbidden request
+ */
 router.get('/galaxies', getGalaxies)
+
 router.post('/galaxy/add', addGalaxy)
 
 module.exports = router
