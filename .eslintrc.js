1aa6d641ce682933d82c4809717dfdee43488078
Mateus Felix <mateus.felix@NOT-M000634.future.corp>
2019-08-16 13:27:19 -0300

    refactor(packages): configure eslint with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,21 @@
 module.exports = {
-  root: true,
-  extends: [
-    'airbnb-base',
-    'plugin:prettier/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 9
-  },
-  env: {
-    node: true
-  },
-  rules: {
-    'semi': ['error', 'never'],
-    'prefer-promise-reject-errors': 'off',
-    'import/no-extraneous-dependencies': ["error", {'devDependencies': ['./src/config/**/*.js']}],
-    'no-plusplus': ["error", { "allowForLoopAfterthoughts": true }],
+    root: true,
+    extends: [
+      'airbnb-base',
+      'plugin:prettier/recommended'
+    ],
+    parserOptions: {
+      ecmaVersion: 9
+    },
+    env: {
+        es6: true,
+        node: true
+    },
+    rules: {
+      'semi': ['error', 'never'],
+      'prefer-promise-reject-errors': 'off',
+      'import/no-extraneous-dependencies': ["error", {'devDependencies': ['./api/config/**/*.js']}],
+      'no-plusplus': ["error", { "allowForLoopAfterthoughts": true }],
+    }
   }
-}
+  
