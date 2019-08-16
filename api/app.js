324c5bcc256dd11db57e178932abc0398eea5ca0
thebinaryfelix <mateus.cfoliveira@gmail.com>
2019-08-15 22:20:09 -0300

    refactor(app): setup morgan and debug

--- api/app.js
+++ api/app.js
@@ -1,14 +1,15 @@
 // Import dependencies
 const express = require('express')
+const morgan = require('morgan')
 const mongoose = require('mongoose')
 const expressSwaggerGenerator = require('express-swagger-generator')
+const debug = require('debug')('app:app.js')
 
 // Import project modules
 const swaggerOptions = require('./config/swagger')
 const routes = require('./routes')
 
 // Configure environment
-const PORT = process.env.PORT
 const DATABASE_URL = process.env.DATABASE_URL
 
 // Setup Express and Swagger
@@ -21,10 +22,14 @@ mongoose
   .connect(DATABASE_URL, {
     useNewUrlParser: true,
   })
-  .then(() => console.log('Connected to mongo!'))
-  .catch(err => console.error('Error connecting to mongo', err))
+  .then(() => debug('Connected to mongo!'))
+  .catch(err => debug(err))
+
+app.use(morgan('dev'))
 
 //  Connect all our routes to the application
 app.use('/', routes)
 
-app.listen(PORT, () => console.log(`Galaxies API listening on port: ${PORT}!`))
+// app.listen(PORT, () => console.log(`Galaxies API listening on port: ${PORT}!`))
+
+module.exports = app
