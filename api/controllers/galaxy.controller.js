68ab99eab845bbf526c53f869a1d9072a5f08f80
thebinaryfelix <mateus.cfoliveira@gmail.com>
2019-08-17 20:55:11 -0300

    refactor(galaxy.routes): done with routest for v.1

--- api/controllers/galaxy.controller.js
+++ api/controllers/galaxy.controller.js
@@ -7,7 +7,7 @@ const getAllGalaxies = async (req, res) => {
     res.status(200).json(galaxies)
   } catch (err) {
     debug(err)
-    res.status(400).json({ message: 'Error getting galaxies.' })
+    res.status(400).json({ message: 'Error getting documents' })
   }
 }
 
@@ -19,30 +19,49 @@ const addGalaxy = async (req, res) => {
     res.status(200).json(newGalaxy)
   } catch (err) {
     debug(err)
-    res.status(400).json({ message: 'Error saving new galaxy.' })
+    res.status(400).json({ message: 'Error saving new document' })
   }
 }
 
-const getGalaxyByCode = async (req, res) => {
+const getGalaxyById = async (req, res) => {
   try {
-    const { code } = req.params
-    const galaxy = await Galaxy.findOne({ code })
-    if (!galaxy) res.status(400).json({ message: 'Galaxy not found.' })
+    const { id } = req.params
+    const galaxy = await Galaxy.findById({ _id: id })
+    if (!galaxy) res.status(400).json({ message: 'Document not found' })
     res.status(200).json(galaxy)
   } catch (err) {
     debug(err)
-    res.status(400).json({ message: 'Galaxy not found.' })
+    res.status(400).json({ message: 'Error getting document' })
   }
 }
 
-// Edit galaxy info
+const editGalaxy = async (req, res) => {
+  try {
+    const { id } = req.params
+    const { catalog, code, name, type, size } = req.body
+    await Galaxy.findByIdAndUpdate({ _id: id }, { catalog, code, name, type, size })
+    res.status(200).json({ message: 'Document edited' })
+  } catch (err) {
+    debug(err)
+    res.status(400).json({ message: 'Error updating document' })
+  }
+}
 
-// Delete galaxy info
-
-// Find galaxies matching query
+const deleteGalaxy = async (req, res) => {
+  try {
+    const { id } = req.params
+    await Galaxy.findByIdAndRemove({ _id: id })
+    res.status(200).json({ message: 'Document deleted' })
+  } catch (err) {
+    debug(err)
+    res.status(400).json({ message: 'Error deleting document' })
+  }
+}
 
 module.exports = {
   getAllGalaxies,
-  getGalaxyByCode,
+  getGalaxyById,
   addGalaxy,
+  editGalaxy,
+  deleteGalaxy,
 }
