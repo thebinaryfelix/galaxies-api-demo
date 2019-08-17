411916e9f1b3102428df73d4ff69c9032c0ffa52
thebinaryfelix <mateus.cfoliveira@gmail.com>
2019-08-17 00:52:46 -0300

    refactor(seeds): add seeds to populate database

---
+++ api/seeds/galaxiesData.js
@@ -0,0 +1,58 @@
+module.exports = [
+  {
+    catalog: 'messier',
+    code: 'm31',
+    name: 'andromeda',
+    size: '220000',
+    type: 'spiral',
+  },
+  {
+    catalog: 'ngc',
+    code: '1',
+    name: '',
+    size: '220000',
+    type: 'spiral',
+  },
+  {
+    catalog: 'ngc',
+    code: '2',
+    name: 'andromeda',
+    size: '220000',
+    type: 'spiral',
+  },
+  {
+    catalog: 'ngc',
+    code: '3',
+    name: '',
+    size: '220000',
+    type: 'lenticular',
+  },
+  {
+    catalog: 'ngc',
+    code: '4',
+    name: '',
+    size: '220000',
+    type: 'lenticular',
+  },
+  {
+    catalog: 'ngc',
+    code: '5',
+    name: '',
+    size: '220000',
+    type: 'elliptical',
+  },
+  {
+    catalog: 'ngc',
+    code: '6',
+    name: '',
+    size: '220000',
+    type: 'lenticular',
+  },
+  {
+    catalog: 'ngc',
+    code: '7',
+    name: '',
+    size: '220000',
+    type: 'spiral',
+  },
+]
