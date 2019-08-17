d2c0e8a09cb30df3a8a893b69aaa622e3c6e1de1
thebinaryfelix <mateus.cfoliveira@gmail.com>
2019-08-17 00:52:03 -0300

    refactor(galaxy.controller): fix typo in catalog, add response to doc returning null

--- api/controllers/galaxy.controller.js
+++ api/controllers/galaxy.controller.js
@@ -1,9 +1,9 @@
 const debug = require('debug')('app:galaxy.controller')
-const GalaxyModel = require('../models/galaxy.model')
+const Galaxy = require('../models/galaxy.model')
 
 const getAllGalaxies = async (req, res) => {
   try {
-    const galaxies = await GalaxyModel.find()
+    const galaxies = await Galaxy.find()
     res.status(200).json(galaxies)
   } catch (err) {
     debug(err)
@@ -12,12 +12,11 @@ const getAllGalaxies = async (req, res) => {
 }
 
 const addGalaxy = async (req, res) => {
-  const { catalogue, code, name, type, size } = req.body
-
   try {
-    const newGalaxy = new GalaxyModel({ catalogue, code, name, type, size })
+    const { catalog, code, name, type, size } = req.body
+    const newGalaxy = new Galaxy({ catalog, code, name, type, size })
     await newGalaxy.save()
-    res.status(201).json({ message: 'A new galaxy was added to database.' })
+    res.status(200).json(newGalaxy)
   } catch (err) {
     debug(err)
     res.status(400).json({ message: 'Error saving new galaxy.' })
@@ -25,10 +24,10 @@ const addGalaxy = async (req, res) => {
 }
 
 const getGalaxyByCode = async (req, res) => {
-  const { code } = req.body
-
   try {
-    const galaxy = await GalaxyModel.findOne({ code })
+    const { code } = req.params
+    const galaxy = await Galaxy.findOne({ code })
+    if (!galaxy) res.status(400).json({ message: 'Galaxy not found.' })
     res.status(200).json(galaxy)
   } catch (err) {
     debug(err)
